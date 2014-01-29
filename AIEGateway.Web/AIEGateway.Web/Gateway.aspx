ff12102bde9157b02fea0245a7688ab34fe56750
Neo <durgac.net@gmail.com>
2014-01-29 01:18:26 -0500

    adding gateway.aspx

---
+++ AIEGateway.Web/AIEGateway.Web/Gateway.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Gateway.aspx.cs" Inherits="AIEGateway.Web.Gateway" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="Scripts/jquery-1.10.2.js"></script>
+    <script src="Scripts/MobileServices.Web-1.0.0.min.js"></script>
+    <script src="Scripts/Gateway.js"></script>
+    <script type="text/javascript" language="javascript">
+        $(document).ready(function () {
+            Gateway.Api.initialize();
+        });
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="errorlog"></div>
+        <ul id="storageLog"></ul>
+        <asp:HiddenField ID="hdnTemperature" runat="server" />
+        <asp:HiddenField ID="hdnWetDry" runat="server" />
+        <footer>
+            <input id="btnClearStorage" type="button" value="Clear Local Storage" />
+        </footer>
+    </form>
+</body>
+</html>
---
+++ AIEGateway.Web/AIEGateway.Web/Gateway.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+using AIEGateway.Web.DataAccess;
+
+namespace AIEGateway.Web
+{
+    public partial class Gateway : System.Web.UI.Page
+    {
+        private Repository _repository = new Repository();
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Execute();
+        }
+
+        private void Execute()
+        {
+            //hdnTemperature.Value = Request["temperature"];
+            //hdnWetDry.Value = Request["wetdry"];
+
+            var temperature = Request["temperature"];
+            var wetDry = Request["wetdry"];
+
+            if (string.IsNullOrEmpty(temperature) && string.IsNullOrEmpty(wetDry))
+                return;
+            
+            var message = (wetDry == "wet") ? "ALERT: Moisture detected" : "ALERT: Temperature threshold exceeded";
+
+            var channels = _repository.GetRegisteredDevices();
+
+            foreach (var channel in channels)
+            {
+                if (string.IsNullOrEmpty(channel))
+                    continue;
+
+                var postData = String.Format("{0} 'temperature': '{1}', 'msg': '{2}', 'channel': '{3}' {4}", "{", temperature, message, channel, "}").Replace("'", "\"");
+                SubmitPostRequest(postData);
+            }
+        }
+
+
+
+        private string SubmitPostRequest(string postData)
+        {
+         // variables to store parameter values
+            string url = "https://aiemobileservice.azure-mobile.net/tables/equipment_incident";
+
+            // create the POST request
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.Method = "POST";
+            webRequest.Headers.Add("X-ZUMO-APPLICATION", "NYuUVUztAwEXJQZxOFbppximTExpoh26");
+            webRequest.ContentType = "application/json; charset=utf-8";
+            webRequest.ContentLength = postData.Length;
+
+            // POST the data
+            using (StreamWriter requestWriter2 = new StreamWriter(webRequest.GetRequestStream()))
+            {
+                requestWriter2.Write(postData);
+            }
+
+            //  This actually does the request and gets the response back
+            HttpWebResponse resp = (HttpWebResponse)webRequest.GetResponse();
+
+            string responseData = string.Empty;
+
+            using (StreamReader responseReader = new StreamReader(webRequest.GetResponse().GetResponseStream()))
+            {
+                // dumps the HTML from the response into a string variable
+                responseData = responseReader.ReadToEnd();
+            }
+            return responseData;
+        }
+
+    }
+}
